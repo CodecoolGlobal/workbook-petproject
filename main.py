eab90c0d957eccbe3ec93c1468715418c28e808a
magfabian <mag.fabian87@gmail.com>
2020-04-27 11:01:18 +0200

    Built main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, get_flashed_messages
 import json
 app = Flask('workbook')
 
 
 @app.route('/')
+@app.route('/index')
 def main_page():
-    return render_template('list.html')
+    return render_template('index.html')
 
 
 def main():
+    app.secret_key = os.urandom(12)
     app.run(debug=True)
 
 
