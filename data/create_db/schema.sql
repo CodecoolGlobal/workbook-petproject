c8681646f0abd6ae03247fab73718dd923a673a6
magfabian <mag.fabian87@gmail.com>
2020-04-27 12:24:15 +0200

    Add schema.sql

---
+++ data/create_db/schema.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS module;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS answer;
+
+
+CREATE TABLE module (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name        VARCHAR(200)        NOT NULL
+);
+
+CREATE TABLE categories (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name        VARCHAR(200)        NOT NULL,
+    module_id   INTEGER             NOT NULL
+);
+
+CREATE TABLE question (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    title       VARCHAR(200)        NOT NULL,
+    module_id   INTEGER             NOT NULL,
+    category_id INTEGER             NOT NULL
+);
+
+CREATE TABLE answer (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    answer      TEXT,
+    module_id   INTEGER             NOT NULL,
+    category_id INTEGER             NOT NULL,
+    question_id INTEGER             NOT NULL
+);
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT fk_categories_module_id FOREIGN KEY (module_id) REFERENCES module(id);
+
+
+ALTER TABLE ONLY question
+    ADD CONSTRAINT fk_question_module_id FOREIGN KEY (module_id) REFERENCES module(id);
+
+
+ALTER TABLE ONLY question
+    ADD CONSTRAINT fk_question_category_id FOREIGN KEY (category_id) REFERENCES categories(id);
+
+
+ALTER TABLE ONLY answer
+    ADD CONSTRAINT fk_answer_module_id FOREIGN KEY (module_id) REFERENCES module(id);
+
+
+ALTER TABLE ONLY answer
+    ADD CONSTRAINT fk_answer_category_id FOREIGN KEY (category_id) REFERENCES categories(id);
+
+
+ALTER TABLE ONLY answer
+    ADD CONSTRAINT fk_answer_question_id FOREIGN KEY (question_id) REFERENCES question(id);
