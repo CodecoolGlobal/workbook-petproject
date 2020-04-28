c800d177ad13d3a46e7177bab9637c0e7e2b6fd8
magfabian <mag.fabian87@gmail.com>
2020-04-28 13:24:06 +0200

    Try to pass selected items to server

--- static/scripts/search_filter.js
+++ static/scripts/search_filter.js
@@ -3,29 +3,47 @@ import {postApi, getApi} from "./data_handler.js";
 const moduleCards = document.querySelectorAll('.module.card__content');
 const categoryCards = document.querySelectorAll('.category.card__content');
 
-
-moduleCards.forEach(moduleCard => {
-    moduleCard.addEventListener('click', event => {
-        moduleCard.classList.toggle('js-selected');
-        let selectedModuleCards = document.querySelectorAll('.module.js-selected');
-        let valueList = [];
-        selectedModuleCards.forEach(card => {
-            valueList.push(card.dataset.module)
-            console.log(valueList)
-            postApi('/search-result', valueList, renderData)
-        })
-    })
+const allCards = document.querySelectorAll('.card__content');
+allCards.forEach(card => {
+    card.classList.toggle('js-selected');
 })
 
-categoryCards.forEach(categoryCard => {
-    categoryCard.addEventListener('click', event => {
-        categoryCard.classList.toggle('js-selected');
+function collectIds() {
+    let searchBy = [];
+
+    const selectedModuleCards = document.querySelectorAll('.module.card__content.js-selected');
+    selectedModuleCards.forEach(selectedCard => {
+        searchBy.push({module_id: selectedCard.dataset.module});
     })
-})
+}
+//
+// moduleCards.forEach(moduleCard => {
+//     moduleCard.addEventListener('click', event => {
+//         moduleCard.classList.toggle('js-selected');
+//         let selectedModuleCards = document.querySelectorAll('.module.js-selected');
+//         let idList = {};
+//         selectedModuleCards.forEach(card => {
+//             idList.module_id = card.dataset.module;
+//             postApi('/search-result', idList, renderData)
+//         })
+//     })
+// })
+//
+// categoryCards.forEach(categoryCard => {
+//     categoryCard.addEventListener('click', event => {
+//         categoryCard.classList.toggle('js-selected');
+//         let idList = {};
+//         let selectedCategoryCards = document.querySelectorAll('.category.js-selected');
+//         selectedCategoryCards.forEach(card => {
+//             idList.category_id = card.dataset.category
+//             postApi('/search-result', idList, renderData)
+//         })
+//     })
+// })
 
 function renderData(data) {
-    console.log(data)
     const resultList = document.querySelector('#result-list');
+
     resultList.innerHTML = '';
     data.forEach(item => {
         resultList.innerHTML +=
