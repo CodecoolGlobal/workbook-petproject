435ff4cd8ded9fec09df8fc2528ae65e1e227f44
magfabian <mag.fabian87@gmail.com>
2020-04-28 11:56:52 +0200

    Add data_handler.js, search_filter.js

---
+++ static/scripts/data_handler.js
@@ -0,0 +1,26 @@
+export function getApi(url, callback) {
+
+    fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+        .then(response => response.json())
+        .then(json_response => callback(json_response));
+}
+
+export function postApi(url, data, callback) {
+    // it is not called from outside
+    // sends the data to the API, and calls callback function
+
+    fetch(url, {
+      method: "POST",
+      headers: { 'Accept': 'application/json',
+          'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+        .then(data => callback(data))
+    ;
+}
+
+
